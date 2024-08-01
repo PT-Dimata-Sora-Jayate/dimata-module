c2c2575bf2148435a1d475103c7c92ef2a4d5928
putuphillipsteven <putu.phillip@gmail.com>
2024-08-01 10:55:09 +0800

    fix(add lesson)

--- html/html-dom/codes/index.js
+++ html/html-dom/codes/index.js
@@ -1,12 +1,26 @@
-// alert('Hello from external script');
+// alert(test);
 
-// const body = document.body;
+// DRY -> Dont repeat yourself
+const body = document.body;
 
-const printText = document.body.querySelector('#print-text');
+// body.addEventListener('keypress', (event) => {
+// 	if (event.key === 'Enter') {
+// 		alert('BODY PRESS');
+// 	}
+// });
 
-const printed = document.body.querySelector('#printed');
-console.log(printed.textContent);
+const printText = body.querySelector('#print-text');
+
+const printed = body.querySelector('#printed');
 
 printText.addEventListener('click', () => {
-	alert(printed.textContent);
+	timpa.classList.remove('hidden');
+});
+
+const timpa = body.querySelector('#timpa');
+
+const buttonTimpa = body.querySelector('#timpa-text');
+
+buttonTimpa.addEventListener('click', () => {
+	timpa.classList.add('hidden');
 });
