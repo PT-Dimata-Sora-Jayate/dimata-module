f6de4489ba380b67e1cdc255ec0a113242376020
putuphillipsteven <putu.phillip@gmail.com>
2024-08-19 09:52:04 +0800

    fix(add some code)

--- javascript/asynchronous/codes/index.js
+++ javascript/asynchronous/codes/index.js
@@ -2,44 +2,27 @@ const a = 1;
 const b = 2;
 const c = 3;
 
-console.log(a);
-setTimeout(() => console.log(b), 1000);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// console.log(a);
+// setTimeout(() => console.log(b), 1000);
+// console.log(c);
 
 let janji = new Promise(function (resolve, reject) {
 	// executor
-	// setTimeout(() => resolve('Done'), 1000);
-	setTimeout(() => reject(new Error('Ops')), 1000);
+	setTimeout(() => resolve('Done'), 1000);
+	// setTimeout(() => reject(new Error('Ops')), 1000);
 });
 
 janji
-	.then((hasil) => {
-		console.log('[INI HASIL]', hasil);
+	.then((user) => {
+		console.log('[INI HASIL]', user);
 	})
-	.catch((error) => {
-		console.log('[INI ERROR]', error);
+	.catch((err) => {
+		console.log('[INI ERROR]', err);
 	})
 	.finally(() => {
-		console.log('Resolve maupun Reject tetap dijalankan');
+		console.log('[INI RESOLVE], Resolve maupun Reject tetap dijalankan');
 	});
-
-function fetchDadJoke() {
-	fetch('https://icanhazdadjoke.com/', {
-		headers: { Accept: 'application/json' },
-	})
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`HTTP error! Status: ${response.status}`);
-			}
-			return response.json();
-		})
-		.then((data) => {
-			console.log(`Dad Joke: ${data.joke}`);
-		})
-		.catch((error) => {
-			console.error('Error fetching dad joke:', error);
-		});
-}
-
-// Call the function to fetch and log a dad joke
-fetchDadJoke();
--- javascript/fetch/codes/index.js
+++ javascript/fetch/codes/index.js
@@ -12,11 +12,35 @@ function fetchDadJoke() {
 		})
 		.then((data) => {
 			console.log(data);
+			console.log(data.id);
 			console.log(`Dad Joke: ${data.joke}`);
 		})
 		.catch((error) => {
 			console.error('Error fetching dad joke:', error);
+		})
+		.finally(() => {
+			console.log('TERIMA KASIH');
 		});
 }
 
 fetchDadJoke();
+
+// function fetchDadJoke2() {
+// 	fetch('https://icanhazdadjoke.com/', {
+// 		headers: { Accept: 'application/json' },
+// 	})
+// 		.then((response) => {
+// 			console.log('[INI RESPONSE]', response);
+// 			if (!response.ok) {
+// 				throw new Error(`HTTP error! Status: ${response.status}`);
+// 			} else {
+// 				return response.json();
+// 			}
+// 		})
+// 		.then((data) => {
+// 			console.log('[BENTUK MENTAH DATA]', data);
+// 		})
+// 		.catch((error) => {
+// 			console.error('Error fetching dad joke:', error);
+// 		});
+// }
